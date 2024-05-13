abaa852edef8eb6c89e0993b70b10f2dcbf49036
AlesciDavide <davi.alesci@email.com>
2024-05-13 13:53:35 +0200

    inserito l'if per il controllo e l'inserimento dei numeri pari

--- saltare a piè disapri/js/script.js
+++ saltare a piè disapri/js/script.js
@@ -2,4 +2,9 @@ let listaNumeri = [];
 
 for(let i = 0; i < 6; i++){
     let numeroInserito = Number.parseInt(prompt('inserisci il numero ' + (i+1)));
+    if( numeroInserito % 2 == 0){
+        listaNumeri.push(numeroInserito);
+    };
 }
+
+console.log(listaNumeri);
