0cc16c4cb81c239aa8ed640884b6fd7643f51369
AlesciDavide <davi.alesci@email.com>
2024-05-13 14:23:26 +0200

    sistemato da 1 a 6 il numero random nel gioco dei dadi

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -1,5 +1,5 @@
-let numeroUno = Math.floor(Math.random() * 10);
-let numeroDue = Math.floor(Math.random() * 10);
+let numeroUno = Math.floor(Math.random() * 6 + 1);
+let numeroDue = Math.floor(Math.random() * 6 + 1);
 
 if(numeroUno > numeroDue){
     console.log('Vince il computer con: ' + numeroUno + '!!');
