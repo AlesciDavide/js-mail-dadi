37c01aee40ec526232c43991b21b3d3e027feb7b
AlesciDavide <davi.alesci@email.com>
2024-05-13 13:33:05 +0200

    inserito array con email e chiesto all'utente la sua email

---
+++ mail/js/script.js
@@ -0,0 +1,6 @@
+const listaemailDb = ['gigilatrottola@gmail.com', 'mariopassasotto@alice.it', 'emanuelepigliafiori@tiscali.it', 'davide.alesci@gmail.com', 'robertotagliacavi@hotmail.it'];
+
+let nomeUtente = prompt('inserisci il tuo nome');
+let nomeUtenteLower = nomeUtente.toLocaleLowerCase();
+
+    
