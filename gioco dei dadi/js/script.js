a8f259582641e885dccaf9848dbb18c9f0b9875b
AlesciDavide <davi.alesci@email.com>
2024-05-13 13:17:44 +0200

    creo un if else per il controllo del maggiore e stampo a console chi ha vinto

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -1,3 +1,8 @@
 let numeroUno = Math.floor(Math.random() * 10);
 let numeroDue = Math.floor(Math.random() * 10);
 
+if(numeroUno > numeroDue){
+    console.log('Vince il computer con: ' + numeroUno + '!!');
+}else{
+    console.log('Complimenti hai visto te con: ' + numeroDue + '!!');
+}
