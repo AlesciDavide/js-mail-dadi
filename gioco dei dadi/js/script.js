7150dc4d516b1ba1d875941197170e957dcd0104
AlesciDavide <davi.alesci@email.com>
2024-05-13 14:53:10 +0200

    aggiunto il pareggio nel gioco dei dadi

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -1,7 +1,9 @@
 let numeroUno = Math.floor(Math.random() * 6 + 1);
 let numeroDue = Math.floor(Math.random() * 6 + 1);
 
-if(numeroUno > numeroDue){
+if (numeroUno == numeroDue){
+    console.log('Avete pareggiato: ' + numeroUno + '!!');
+}else if(numeroUno > numeroDue){
     console.log('Vince il computer con: ' + numeroUno + '!!');
 }else{
     console.log('Complimenti hai visto te con: ' + numeroDue + '!!');
